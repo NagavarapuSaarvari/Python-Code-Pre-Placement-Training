6ca221f1f03c8204d153c52c1bad2b14baa309dc
NagavarapuSaarvari <149094209+NagavarapuSaarvari@users.noreply.github.com>
2025-07-15 11:19:38 +0530

    15 July 2025

--- Test.py
+++
@@ -1,9 +0,0 @@
-'''for i,j in [[10,20],[30,40]]:
-    print(i+j)'''
-for i in range(6):
-    print("*",end=' ')
-print()
-for i in range(1,5):
-    print("*         *")
-for i in range(6):
-    print("*",end=' ')
